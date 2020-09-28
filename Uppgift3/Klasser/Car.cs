6a4ef3830ec7acb7fad38215a723f50ce20046bd
albin <albin@DESKTOP-3FGFIIK>
2020-09-28 13:11:08 +0200

    Klar
    Istället för att all information skrivs ut så gjorde jag så att bara information kring personen som användaren väljer skrivs ut.

---
+++ Uppgift3/Klasser/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Klasser
+{
+    public class Car
+    {
+        public string Reg { get; set; }
+        public string Model { get; set; }
+        public string Electric { get; set; }
+        public int Weight { get; set; }
+      
+
+        public DateTime RegisterDate { get; set; }
+
+        public Car(string model, string reg, int weight, bool electric)
+        {
+            
+
+            Model = model;
+            Reg = reg;
+            Weight = weight;
+
+            if (electric)
+            {
+                Electric = "Det är en elbil";
+            }
+            else 
+            {
+                Electric = "Det är inte en elbil";
+            }   
+            
+            RegisterDate = DateTime.Now;
+        }
+
+       
+
+    }
+}
